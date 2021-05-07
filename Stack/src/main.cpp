914ee91456b143c78b0fa00a03bca818079cb909
MohamedFouadIbrahim <mohamed_fouad617@yahoo.com>
2021-05-07 22:26:58 +0200

    selectionSort

--- Stack/src/main.cpp
+++ Stack/src/main.cpp
@@ -251,6 +251,35 @@ template <class t>
 
 #pragma endregion
 
+#pragma region // selectionSort
+
+void selectionSort(int arr[]);
+
+void selectionSort(int arr[], int length)
+{
+
+    int t, smallestElementIndex = 0;
+
+    while (t + 1 < length)
+    {
+        for (int i = t; i < length; i++) // find small one
+        {
+            if (arr[smallestElementIndex] > arr[i])
+            {
+                smallestElementIndex = i;
+            }
+        }
+
+        // replace it at the first
+        int temp = arr[t];
+        arr[t] = arr[smallestElementIndex];
+        arr[smallestElementIndex] = temp;
+        t++;
+        smallestElementIndex = t;
+    }
+}
+
+#pragma endregion
 
 int main(int argc, char *argv[])
 {
--- Stack/src/tempCodeRunnerFile.cpp
+++ Stack/src/tempCodeRunnerFile.cpp
@@ -1,4 +1,1 @@
-.push(8);
-	// fo.push(9);
-	// fo.push(10);
-	// fo.print();
+smallestElement
